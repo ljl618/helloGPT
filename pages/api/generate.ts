81ff3c65064c18a7ed1de35f7d957010c57b0fc7
llj <liuliangji@bjsasc.com>
2023-03-17 18:24:46 +0800

    change function type edge 2

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -14,18 +14,20 @@ export const config = {
 };
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+    req: Request
 ): Promise<Response> {
     
     // owner api key proxy open ai service
     if (req.method === 'POST') {
-        const postData = JSON.parse(req.body) as PostData;
-        const apiKey = JSON.parse(req.body).apiKey || process.env.API_KEY;
+        const postData = (await req.json()) as {
+            apiKey: string;
+            messages: IMessage[];
+        }
+        // const apiKey = JSON.parse(req.body).apiKey || process.env.API_KEY;
         const options = {
             headers: {
                 'Content-Type': 'application/json',
-                Authorization: `Bearer ${apiKey}`,
+                Authorization: `Bearer ${postData.apiKey}`,
             },
             method: 'POST',
             timeout: 8000,
@@ -42,55 +44,8 @@ export default async function handler(
         );
 
         return new Response(parseOpenAIStream(proxyRes));
-        // try {
-        //     const proxyRes = await fetch(
-        //         `https://api.openai.com/v1/chat/completions`,
-        //         options
-        //     );
-        //     if (!proxyRes.body) {
-        //         res.status(500).json({ message: 'Internal Server Error' });
-        //         return;
-        //     }
-
-        //     proxyRes.body.on('data', (chunk) => {
-        //         // Real-time writing data into the response stream and sending it to the client.
-        //         res.write(chunk);
-        //     });
-
-        //     proxyRes.body.on('end', () => {
-        //         // Send an end signal to the client when the response stream ends.
-        //         res.end();
-        //     });
-        // } catch (error) {
-        //     res.status(500).json({ message: 'Internal Server Error' });
-        // }
+     
     } else {
-        res.status(405).end();
         return new Response('Method Not Allowed', { status: 405 });
     }
 }
-
-
-// try {
-//     const res = await fetch(`/api/chat_with_gpt_by_proxy`, {
-//         method: 'POST',
-//         body: JSON.stringify({
-//             apiKey,
-//             messages: messages.map((item) => ({
-//                 role: item.role,
-//                 content: item.content,
-//             })),
-//         }),
-//         signal: controller.signal,
-//     }).then(async (response) => {
-//         if (!response.ok) {
-//             const text = await response.text();
-//             console.log('错误--', text, typeof text);
-//             throw JSON.parse(text);
-//         }
-//         return response;
-//     });
-//     return new Response(parseOpenAIStream(res));
-// } catch (error) {
-//     throw error;
-// }
